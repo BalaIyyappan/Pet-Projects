fa8679430fe60a6b61e1da354bfb866ce604ced9
Bala Iyyappan V <43007978+BalaIyyappan@users.noreply.github.com>
2020-03-03 23:49:13 +0530

    Update index.js

--- Tic Tac Toe/index.js
+++ Tic Tac Toe/index.js
@@ -1,4 +1,5 @@
 let playerid=document.getElementById("player");
+
 window.onload=function(){
 	let divbox=document.getElementById("box");
 	for(i=1;i<10;i++){
@@ -17,14 +18,14 @@ window.onload=function(){
 		if(this.innerHTML!=="X"&&this.innerHTML!=="O"){
 		if(X_or_O%2===0){
 			// console.log(X_or_O);
-			playerid.innerHTML="O Plays Now";
+			playerid.innerHTML="O Turn Now";
 			this.innerHTML="X";
 			game();
 			X_or_O+=1;
 		}
 		else{
 			// console.log(X_or_O);
-			playerid.innerHTML="X Plays Now";
+			playerid.innerHTML="X Turn Now";
 			this.innerHTML="O";
 			game();
 			X_or_O+=1;
@@ -41,28 +42,28 @@ function game(){
 	let div7=document.getElementById("box7");let div8=document.getElementById("box8");let div9=document.getElementById("box9");
 	//To predict winner
 	if(div1.innerHTML!==""&&div1.innerHTML===div2.innerHTML&&div1.innerHTML===div3.innerHTML){
-		console.log("Winner");winner();
+		highlight(div1,div2,div3); winner(div1.innerHTML);
 	}
 	else if(div4.innerHTML!==""&&div4.innerHTML===div5.innerHTML&&div4.innerHTML===div6.innerHTML){
-		console.log("Winner");winner();
+		highlight(div4,div5,div6);winner(div4.innerHTML);
 	}
 	else if(div7.innerHTML!==""&&div7.innerHTML===div8.innerHTML&&div7.innerHTML===div9.innerHTML){
-		console.log("Winner");winner();
+		highlight(div7,div8,div9);winner(div7.innerHTML);
 	}
 	else if(div1.innerHTML!==""&&div1.innerHTML===div4.innerHTML&&div1.innerHTML===div7.innerHTML){
-		console.log("Winner");winner();
+		highlight(div1,div4,div7);winner(div1.innerHTML);
 	}
 	else if(div2.innerHTML!==""&&div2.innerHTML===div5.innerHTML&&div2.innerHTML===div8.innerHTML){
-		console.log("Winner");winner();
+		highlight(div2,div5,div8);winner(div2.innerHTML);
 	}
 	else if(div3.innerHTML!==""&&div3.innerHTML===div6.innerHTML&&div3.innerHTML===div9.innerHTML){
-		console.log("Winner");winner();
+		highlight(div3,div6,div9);winner(div3.innerHTML);
 	}
 	else if(div1.innerHTML!==""&&div1.innerHTML===div5.innerHTML&&div1.innerHTML===div9.innerHTML){
-		console.log("Winner");winner();
+		highlight(div1,div5,div9);winner(div1.innerHTML);
 	}
 	else if(div3.innerHTML!==""&&div3.innerHTML===div5.innerHTML&&div3.innerHTML===div7.innerHTML){
-		console.log("Winner");winner();
+		highlight(div3,div5,div7);winner(div3.innerHTML);
 	}
 	else{
 		console.log("Tie!");
@@ -70,14 +71,26 @@ function game(){
 }
 
 function foo(){
-	let div1=document.getElementById("box1");let div2=document.getElementById("box2");let div3=document.getElementById("box3");
-	let div4=document.getElementById("box4");let div5=document.getElementById("box5");let div6=document.getElementById("box6");
-	let div7=document.getElementById("box7");let div8=document.getElementById("box8");let div9=document.getElementById("box9");
-	div1.innerHTML="";div2.innerHTML="";div3.innerHTML="";div4.innerHTML="";div5.innerHTML="";div6.innerHTML="";div7.innerHTML="";
-	div8.innerHTML="";div9.innerHTML="";
+// 	let div1=document.getElementById("box1");let div2=document.getElementById("box2");let div3=document.getElementById("box3");
+// 	let div4=document.getElementById("box4");let div5=document.getElementById("box5");let div6=document.getElementById("box6");
+// 	let div7=document.getElementById("box7");let div8=document.getElementById("box8");let div9=document.getElementById("box9");
+// 	div1.innerHTML="";div2.innerHTML="";div3.innerHTML="";div4.innerHTML="";div5.innerHTML="";div6.innerHTML="";div7.innerHTML="";
+// 	div8.innerHTML="";div9.innerHTML="";resultclear();
+// }
+	location.reload();
+}
+function winner(x_or_o){
+	let winner=document.getElementById("result");
+	winner.innerHTML=x_or_o+" is Winner";
 }
 
-function winner(){
-	let winner=document.getElementById("result");
-	winner.innerHTML="Winner"
+// function resultclear(){
+// 	let cls=document.getElementById("result");
+// 	cls.innerHTML="";
+// }
+
+function highlight(a,b,c){
+	a.style.background='#e33b5a';
+	b.style.background="#e33b5a";
+	c.style.background="#e33b5a";
 }
